4288353281f3349172e8336206c36dd692b2db6f
Anush Muradyan <>
2021-09-27 19:23:21 +0400

    initial project

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,27 +1,29 @@
-using DG.Tweening;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Ball : MonoBehaviour {
+    [SerializeField] private Rigidbody2D rigidbody2D;
     [SerializeField] private Camera camera;
-    [SerializeField] private float jumpPower=1f;
-    [SerializeField] private float duration=2f;
-    private Vector3 startPosition;
-    private bool jumping;
-
-    private void Start() {
-        startPosition = transform.position;
-    }
-
+    
     private void Update() {
-        if (Input.GetMouseButtonDown(0) && !jumping) {
-            jumping = true;
-            Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-            transform.DOJump(mousePosition, jumpPower, 1, duration).OnComplete(jumpComplete);
+        Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        var dir = mousePosition - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Debug.DrawLine(transform.position, mousePosition, Color.red);
+        if (!Input.GetMouseButtonDown(0)) {
+            return;
         }
-    }
 
-    private void jumpComplete() {
-        transform.position = startPosition;
-        jumping = false;
+        var g = Mathf.Abs(Physics2D.gravity.y);
+
+        var H = mousePosition.y - transform.position.y - transform.localScale.y * 0.5f;
+
+        var v0 = (Mathf.Sqrt(2f * g * H)) / Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        var v0x = v0 * Mathf.Cos(angle * Mathf.Deg2Rad);
+        var v0y = v0 * Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        var velocity = new Vector2(v0x, v0y);
+        rigidbody2D.velocity = velocity;
     }
 }
